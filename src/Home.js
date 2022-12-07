1125cddebcd271f2b3751fb26ee0446a5e33c537
lrakoto <lova@threeohfivestudios.com>
2022-12-06 19:27:23 -0800

    fixed projects button

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,7 @@ function Home() {
                 <p class="lead spread1">DEVELOPER <span class="text-primary">|</span> DESIGNER <span class="text-primary">|</span> GAMER</p>
                 <p class="lead">Thanks for visiting this page. Here you'll find a collection of my most recent work.</p>
                 <p class="lead">
-                    <form action="/projects">
-                        <button action="submit" class="btn btn-lg btn-primary fw-bold my-3">View Projects</button>
-                    </form>
+                        <a href="/projects"><button action="submit" class="btn btn-lg btn-primary fw-bold my-3">View Projects</button></a>
                 </p>
             </main>
         </div>
