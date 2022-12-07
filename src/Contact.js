1d2a018ee11ce8430397de3c4e9d0bfeafbaace1
lrakoto <lova@threeohfivestudios.com>
2022-12-06 19:18:54 -0800

    removed blog link

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <h1>Contact</h1>
+                    <p>Welcome to the Contact page</p>                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
